0238c778040e10f82a01fff12f14521f82fc63ea
Harold García <supapito85@gmail.com>
2024-11-18 19:10:41 -0500

    categoria

--- conexion.py
+++ conexion.py
@@ -10,5 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 db = mysql.connector.connect(host='localhost', port='3306',  user='root', password='', database='mercasena')
+#db = mysql.connector.connect(host='localhost', port='3306',  user='arcano_operador', password='7Uxn)v5)wt26Op_f', database='mercasena')
+
 
 app.config['CARPETAU'] = os.path.join('uploads')
